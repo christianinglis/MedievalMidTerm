6908ca9fca94664b1a281e36cb2d816e2a80cf6f
christianinglis <ccinglis@my.waketech.edu>
2026-03-10 11:30:44 -0400

    js buttons

---
+++ script.js
@@ -0,0 +1,14 @@
+let p1 = document.getElementById("paragraph1")
+let p2 = document.getElementById("paragraph2")
+let p3 = document.getElementById("paragraph3")
+
+let graveyardButton = document.getElementById("graveyardButton")
+let basementButton = document.getElementById("basementButton")
+let castleButton = document.getElementById("castleButton")
+
+let fightButton = document.getElementById("fightButton")
+let bribeButton = document.getElementById("bribeButton")
+let friendlyButton = document.getElementById("friendlyButton")
+let backButton = document.getElementById("backButton")
+
+let submitGuessButton = document.getElementById("submitGuessButton")
